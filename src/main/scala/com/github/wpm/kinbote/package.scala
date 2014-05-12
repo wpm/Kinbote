e4ad9be93a3ce6b4dfd349cf4166b3dda80be945
W.P. McNeill <billmcn@gmail.com>
2014-05-11 19:12:56 -0500

    Remove StandoffAnnotations class
    This is superseded by Annotations.

--- src/main/scala/com/github/wpm/kinbote/package.scala
+++ src/main/scala/com/github/wpm/kinbote/package.scala
@@ -7,20 +7,20 @@ package object kinbote {
   type Offset = Int
 
   /**
-   * An annotator adds to a document's standoff annotations.
+   * An annotator adds to a document's annotations.
    */
   trait Annotator {
-    def annotate(document: Document, annotations: StandoffAnnotation): StandoffAnnotation
+    def annotate(document: Document, annotations: Annotations): Annotations
   }
 
   /**
    * A sequence of annotators to apply to a document
    *
-   * Each annotator may make reference to the annotations created by the previous annotators.
+   * Each annotator may make use of the annotations created by the previous annotators.
    * @param annotators sequence of annotators
    */
   case class AnnotatorChain(annotators: Annotator*) {
-    def annotate(document: Document): StandoffAnnotation =
-      (StandoffAnnotation() /: annotators)((as, a) => a.annotate(document, as))
+    def annotate(document: Document): Annotations = (Annotations() /: annotators)((as, a) => a.annotate(document, as))
   }
+
 }
